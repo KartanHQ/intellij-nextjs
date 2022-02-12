e685abdbc4ab10feca852323ab5f75924cae4dea
Milad Nekofar <milad@nekofar.com>
2022-02-12 12:53:04 +0330

    feat: implement project generation through `create-next-app`
    fixup! feat: implement project generation through `create-next-app`
    
    fixup! feat: implement project generation through `create-next-app`

---
+++ src/main/kotlin/com/nekofar/milad/intellij/nextjs/NextBundle.kt
@@ -0,0 +1,13 @@
+package com.nekofar.milad.intellij.nextjs
+
+import com.intellij.DynamicBundle
+import org.jetbrains.annotations.NonNls
+import org.jetbrains.annotations.PropertyKey
+
+@NonNls
+private const val BUNDLE = "messages.NextBundle"
+
+object NextBundle: DynamicBundle(BUNDLE) {
+    @JvmStatic
+    fun message(@PropertyKey(resourceBundle = BUNDLE) key: String, vararg params: Any) = getMessage(key, *params)
+}
---
+++ src/main/kotlin/com/nekofar/milad/intellij/nextjs/NextIcons.kt
@@ -0,0 +1,8 @@
+package com.nekofar.milad.intellij.nextjs
+
+import com.intellij.openapi.util.IconLoader
+
+object NextIcons {
+    @JvmField
+    val ProjectGenerator = IconLoader.getIcon("/icons/next.svg", javaClass)
+}
