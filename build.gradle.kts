c2a7842ce2910f87141774d0ed1c7f44246a6ac1
Milad Nekofar <milad@nekofar.com>
2022-03-03 10:58:38 +0330

    build: add a static import for `markdownToHTML`

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,4 @@
+import org.jetbrains.changelog.markdownToHTML
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 fun properties(key: String) = project.findProperty(key).toString()
@@ -88,7 +89,7 @@ tasks {
                     throw GradleException("Plugin description section not found in README.md:\n$start ... $end")
                 }
                 subList(indexOf(start) + 1, indexOf(end))
-            }.joinToString("\n").run { org.jetbrains.changelog.markdownToHTML(this) }
+            }.joinToString("\n").run { markdownToHTML(this) }
         )
 
         // Get the latest available change notes from the changelog file
@@ -127,6 +128,7 @@ tasks {
         channels.set(listOf(properties("pluginVersion").split('-').getOrElse(1) { "default" }.split('.').first()))
     }
 }
+
 tasks.getByName<Test>("test") {
     useJUnitPlatform()
 }
