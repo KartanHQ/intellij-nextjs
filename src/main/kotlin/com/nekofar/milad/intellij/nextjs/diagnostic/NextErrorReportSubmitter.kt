ae8657d9673cd5d543696e09cf54469201228570
Milad Nekofar <milad@nekofar.com>
2022-03-25 22:25:50 +0430

    refactor: add `NextErrorReportSubmitter` as `errorHandler`

---
+++ src/main/kotlin/com/nekofar/milad/intellij/nextjs/diagnostic/NextErrorReportSubmitter.kt
@@ -0,0 +1,9 @@
+package com.nekofar.milad.intellij.nextjs.diagnostic
+
+import com.intellij.openapi.diagnostic.ErrorReportSubmitter
+
+class NextErrorReportSubmitter : ErrorReportSubmitter() {
+    override fun getReportActionText(): String {
+        TODO("Not yet implemented")
+    }
+}
